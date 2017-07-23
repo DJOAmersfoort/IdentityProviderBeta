b577fe5da30fd486fcd5c45fd7f44145458e4de7
Roelof Roos <github@roelof.io>
2017-07-23 16:52:43 +0200

    Added javascript, scss and images with Encore

---
+++ app/Resources/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Main Javascript for DJO Identity Provider
+ *
+ * @author Roelof Roos
+ */
+
+// Always use IIFE
+(function () {
+  'use strict'
+
+  let init = () => {
+
+  }
+
+  document.addEventListener('DOMDocumentReady', init)
+}())
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/**
+ * Encore configuration, handles creating all static assets
+ *
+ * @author Roelof Roos
+ */
+
+// webpack.config.js
+const Encore = require('@symfony/webpack-encore')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin').default
+const ImageminMozjpeg = require('imagemin-mozjpeg')
+
+Encore
+  // directory where all compiled assets will be stored
+  .setOutputPath('web/public/')
+
+  // what's the public path to this directory (relative to your project's document root dir)
+  .setPublicPath('/public')
+
+  // empty the outputPath dir before each build
+  .cleanupOutputBeforeBuild()
+
+  // will output as web/build/app.js
+  .addEntry('app', './app/Resources/js/main.js')
+
+  // will output as web/build/main.css
+  .addStyleEntry('main', './app/Resources/scss/main.scss')
+
+  // allow sass/scss files to be processed
+  .enableSassLoader()
+
+  // Enable source maps on production
+  .enableSourceMaps(!Encore.isProduction())
+
+  // create hashed filenames (e.g. app.abc123.css)
+  .enableVersioning()
+
+  // Enable PostCSS processing
+  .enablePostCssLoader()
+
+  // Copy images to destination
+  .addPlugin(new CopyWebpackPlugin([
+    { from: './app/Resources/images/*.{jpg,jpeg,png,gif,svg}' }
+  ]))
+
+  // Apply imagemin, using mozjpeg for JPEG minification
+  .addPlugin(new ImageminPlugin({
+    disable: !Encore.isProduction(),
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    plugins: [
+      ImageminMozjpeg({
+        quality: 90,
+        progressive: true
+      })
+    ]
+  }))
+
+// export the final configuration
+module.exports = Encore.getWebpackConfig()
