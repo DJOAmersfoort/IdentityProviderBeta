5f6b9bcc551be6182da4c414b329c789e67ace85
Roelof Roos <github@roelof.io>
2017-08-13 12:10:15 +0200

    Added FaviconController

---
+++ src/AppBundle/Controller/FaviconController.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Responds with manifest files and other files used to render the icons
+ * properly
+ *
+ * @author Roelof Roos
+ */
+class FaviconController extends Controller
+{
+    /**
+     * Returns the manifest file, which contains information for Evergreen browsers
+     *
+     * @return Response
+     */
+    public function manifestAction() : Response
+    {
+        $expire = new \DateTime();
+        $expire->add(new \DateInterval('P14D'));
+
+        return $this
+            ->render('favicon/manifest.json.twig')
+            ->setDate(new \DateTime)
+            ->setPublic()
+            ->setExpires($expire);
+    }
+
+    /**
+     * Returns the browserconfig file, which contains information for Edge and
+     * Internet Exploder.
+     *
+     * @return Response
+     */
+    public function browserConfigAction() : Response
+    {
+        $expire = new \DateTime();
+        $expire->add(new \DateInterval('P14D'));
+
+        return $this
+            ->render('favicon/browserconfig.xml.twig')
+            ->setDate(new \DateTime)
+            ->setPublic()
+            ->setExpires($expire);
+    }
+}
